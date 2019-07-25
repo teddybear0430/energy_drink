8f14e5bb5fa4acde1b3e806e83977350a40454aa
teddybear0430 <teddybear0430@hotmail.com>
2019-07-25 23:50:13 +0900

    エナジードリンクの配列をApp.jsに移動

--- src/App.js
+++ src/App.js
@@ -1,16 +1,95 @@
 import React from 'react';
 import './css/main.css';
 import Header from './Header';
+import SlideNav from './SlideNav';
 import Main from './Main';
 import Footer from './Footer';
 
 class App extends React.Component {
   render() {
-    return (
+    const drink_data = [
+      {
+          name: 'モンスターエナジー（日本）',
+          capa: 355,
+          cal: 178,
+          caffeine: 142
+      },
+      {
+          name: 'モンスターエナジー（米）',
+          capa: 473,
+          cal: 210,
+          caffeine: 160
+      },
+      {
+          name: 'モンスターエナジー M3',
+          capa: 150,
+          cal: 66,
+          caffeine: 150
+      },
+      {
+          name: 'レッドブル（日本）',
+          capa: 185,
+          cal: 85,
+          caffeine: 80
+      },
+      {
+          name: 'レッドブル（米国）',
+          capa: 250,
+          cal: 110,
+          caffeine: 80
+      },
+      {
+          name: 'ロックスターエナジー（日本）',
+          capa: 250,
+          cal: 145,
+          caffeine: 120
+      },
+      {
+          name: 'ロックスターエナジー（米）',
+          capa:　473,
+          cal: 280,
+          caffeine: 160
+      },
+      {
+          name: 'コカ･コーラ エナジー',
+          capa:　250,
+          cal: 105,
+          caffeine: 80
+      },
+      {
+          name: 'エナジードリンク（西友）',
+          capa:　250,
+          cal: 128,
+          caffeine: 160
+      },
+      {
+          name: 'ブラックアウトゼロ',
+          capa:　500,
+          cal: 0,
+          caffeine: 175
+      },
+      {
+          name: 'ブラックアウトゴッド',
+          capa:　250,
+          cal: 148,
+          caffeine: 160
+      },
+      {
+          name: 'エナジーハンター',
+          capa:　300,
+          cal: 162,
+          caffeine: 195
+      }
+  ];
+  
+  return (
       <div>
+        <SlideNav />
+        <div id="main">
         <Header />
-        <Main />
+        <Main data={drink_data}/>
         <Footer />
+        </div>
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -5,85 +5,10 @@ import TableArea from './main/TableArea';
 
 class Main extends React.Component {
     render() {
-        const drink_data = [
-            {
-                name: 'モンスターエナジー（日本）',
-                capa: 355,
-                cal: 178,
-                caffeine: 142
-            },
-            {
-                name: 'モンスターエナジー（米）',
-                capa: 473,
-                cal: 210,
-                caffeine: 160
-            },
-            {
-                name: 'モンスターエナジー M3',
-                capa: 150,
-                cal: 66,
-                caffeine: 150
-            },
-            {
-                name: 'レッドブル（日本）',
-                capa: 185,
-                cal: 85,
-                caffeine: 80
-            },
-            {
-                name: 'レッドブル（米国）',
-                capa: 250,
-                cal: 110,
-                caffeine: 80
-            },
-            {
-                name: 'ロックスターエナジー（日本）',
-                capa: 250,
-                cal: 145,
-                caffeine: 120
-            },
-            {
-                name: 'ロックスターエナジー（米）',
-                capa:　473,
-                cal: 280,
-                caffeine: 160
-            },
-            {
-                name: 'コカ･コーラ エナジー',
-                capa:　250,
-                cal: 105,
-                caffeine: 80
-            },
-            {
-                name: 'エナジードリンク（西友）',
-                capa:　250,
-                cal: 128,
-                caffeine: 160
-            },
-            {
-                name: 'ブラックアウトゼロ',
-                capa:　500,
-                cal: 0,
-                caffeine: 175
-            },
-            {
-                name: 'ブラックアウトゴッド',
-                capa:　250,
-                cal: 148,
-                caffeine: 160
-            },
-            {
-                name: 'エナジーハンター',
-                capa:　300,
-                cal: 162,
-                caffeine: 195
-            }
-        ];
-
         return(
             <div class="container">
-                <ChartArea data={drink_data} />
-                <TableArea data={drink_data} />
+                <ChartArea data={this.props.data} />
+                <TableArea data={this.props.data} />
             </div>
         );
     }
