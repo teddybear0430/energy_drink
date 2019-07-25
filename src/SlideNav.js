9d55504ed7b6d4d4fcc91656acb5b087ad285290
teddybear0430 <teddybear0430@hotmail.com>
2019-07-25 23:50:50 +0900

    ナビのコンポーネントを作成

---
+++ src/SlideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SlideNav extends React.Component {
+    render() {
+        return (
+            <div id="slide-nav"></div>
+        );
+    }
+}
+
+export default SlideNav;
