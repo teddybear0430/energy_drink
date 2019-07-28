411fc9d3f06a0135cf8d4be813ab6384e133e7a6
teddybear0430 <teddybear0430@hotmail.com>
2019-07-28 23:43:24 +0900

    Linkをインポート。

--- src/SlideNav.js
+++ src/SlideNav.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SlideNav extends React.Component {
     render() {
         return (
             <div id="slide-nav">
                 <ul>
+                    <li><Link to="/">TOP</Link></li>
                     {this.props.data.map((drink) => {
                         return (
-                            <li>{drink.name}</li>
+                            <li><Link to="/data">{drink.name}</Link></li>
                         );
                     })}
                 </ul>
