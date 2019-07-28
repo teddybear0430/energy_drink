305352e969fa8d2424c896bb9186cef7450ceb4c
teddybear0430 <teddybear0430@hotmail.com>
2019-07-28 23:41:29 +0900

    レーダーチャート用のコンポーネントを作成。

---
+++ src/Data.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Data extends React.Component {
+    render() {
+        return(
+            <div class="container">
+                <span>TEST</span>
+            </div>
+        );
+    }
+}
+
+export default Data;
