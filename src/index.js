79456ba464f49d2a253f8ffa8ea3ca73d3e45357
teddybear0430 <teddybear0430@hotmail.com>
2019-07-30 00:45:15 +0900

    createStore,Provider,reducerをインポート。

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import reducer from './reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
+    <Provider store={store}>
     <Router>
         <App />
-    </Router>, 
+    </Router>
+    </Provider>, 
     document.getElementById('root')
 );
