0f0010a9e70aa7cfd1c23799246efe21e98f202a
teddybear0430 <teddybear0430@hotmail.com>
2019-08-03 22:43:40 +0900

    オブジェクトの順番を並び替え。

--- src/drinkData.js
+++ src/drinkData.js
@@ -1,124 +1,123 @@
 const drinkData = [
     {
         name: 'モンスターエナジー（日本）',
-        capa: 355,
         cal: 178,
         caffeine: 142,
         carb: 45,
         arginine: 444,
         bcaa: 0,
-        niacin: 30,
-        
+        niacin: 30, 
+        capa: 355
     },
     {
         name: 'モンスターエナジー（米）',
-        capa: 473,
         cal: 210,
         caffeine: 160,
         carb: 54,
         arginine: 0,
         bcaa: 0,
-        niacin: 22,       
+        niacin: 22, 
+        capa: 473     
     },
     {
         name: 'モンスターエナジー M3',
-        capa: 150,
         cal: 66,
         caffeine: 150,
         carb: 16,
         arginine: 450,
         bcaa: 0,
-        niacin: 0,      
+        niacin: 0,  
+        capa: 150 
     },
     {
         name: 'レッドブル（日本）',
-        capa: 185,
         cal: 85,
         caffeine: 80,
         carb: 20,
         arginine: 220,
         bcaa: 0,
-        niacin: 6,      
+        niacin: 6,
+        capa: 185 
     },
     {
         name: 'レッドブル（米国）',
-        capa: 250,
         cal: 110,
         caffeine: 80,
         carb: 28,
         arginine: 0,
         bcaa: 0,
-        niacin: 11,     
+        niacin: 11,
+        capa: 250   
     },
     {
         name: 'ロックスターエナジー（日本）',
-        capa: 250,
         cal: 145,
         caffeine: 120,
         carb: 32,
         arginine: 300,
         bcaa: 0,
-        niacin: 20,      
+        niacin: 20,
+        capa: 250      
     },
     {
         name: 'ロックスターエナジー（米）',
-        capa:　473,
         cal: 280,
         caffeine: 160,
         carb: 64,
         arginine: 0,
         bcaa: 0,
-        niacin: 22,     
+        niacin: 22,
+        capa:　473 
     },
     {
         name: 'コカ･コーラ エナジー',
-        capa:　250,
         cal: 105,
         caffeine: 80,
         carb: 42,
         arginine: 0,
         bcaa: 0,
-        niacin: 4,      
+        niacin: 4,
+        capa:　250    
     },
     {
         name: 'エナジードリンク（西友）',
-        capa:　250,
         cal: 128,
         caffeine: 160,
         carb: 31,
         arginine: 450,
         bcaa: 0,
-        niacin: 9,        
+        niacin: 9,
+        capa:　250    
     },
     {
-        name: 'ブラックアウトゼロ',
-        capa:　500,
-        cal: 0,
-        caffeine: 175,
-        carb: 0,
+        name: 'ブラックアウト',
+        cal: 245,
+        caffeine: 190,
+        carb: 60,
         arginine: 600,
         bcaa: 0,
-        niacin: 4,      
+        niacin: 7,
+        capa:　500    
     },
     {
         name: 'ブラックアウトゴッド',
-        capa:　250,
         cal: 148,
         caffeine: 160,
         carb: 35,
         arginine: 550,
         bcaa: 0,
-        niacin: 8,    
+        niacin: 8,
+        capa:　250  
     },
     {
         name: 'エナジーハンター',
-        capa:　300,
         cal: 162,
         caffeine: 195,
         carb: 32,
         arginine: 600,
         bcaa: 0,
-        niacin: 0,      
+        niacin: 0,
+        capa:　300      
     }
 ];
 
