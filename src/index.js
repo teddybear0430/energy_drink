c9823475f5a54c08f5bfb527904f7f8fc534841d
teddybear0430 <teddybear0430@hotmail.com>
2019-08-03 22:44:18 +0900

    不要なコードを削除。

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reducer from './reducer';
-
-const store = createStore(reducer);
 
 ReactDOM.render(
-    <Provider store={store}>
     <Router>
         <App />
-    </Router>
-    </Provider>, 
+    </Router> , 
     document.getElementById('root')
 );
